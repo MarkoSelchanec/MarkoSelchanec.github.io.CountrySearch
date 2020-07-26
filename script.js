6b707ae6a1f6e6ca71fc40bc98a49cee13867138
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-07-26 11:50:53 +0200

    working on content page

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const title = document.getElementsByClassName("title")[0];
 const link = "https://restcountries.eu/rest/v2/all";
 
+const countryName = document.getElementById("country-name");
+const countryFlag = document.getElementById("country-flag");
+const countryInfo = document.getElementsByClassName("country-info")[0].getElementsByTagName("p");
+const countryCurrency = document.getElementsByClassName("currency")[0];
+const countryBorders = document.getElementsByClassName("borders-div")[0];
+
 let data = [];
 
 let getData = async (link) => {
@@ -36,14 +42,50 @@ for (let i = 0; i < titleLetters.length; i++) {
 
 // // Content page
 setTimeout(() => {
-    let img = document.getElementsByClassName('test')[0];
-    let par = document.getElementsByTagName("p")
-    img.setAttribute("src", data[0].flag)
-    par[0].innerText = data[0].capital
-    par[1].innerText = data[0].currencies[0].name;
-    data[0].languages.forEach(e => par[2].innerText += e.name + "\n")
-    par[3].innerText = data[0].population.toLocaleString();
+    // let img = document.getElementsByClassName('test')[0];
+    // let par = document.getElementsByTagName("p")
+    // img.setAttribute("src", data[0].flag)
+    // par[0].innerText = data[0].capital
+    // par[1].innerText = data[0].currencies[0].name;
+    // data[0].languages.forEach(e => par[2].innerText += e.name + "\n")
+    // par[3].innerText = data[0].population.toLocaleString();
+
+    countryName.innerHTML = data[0].name
+    countryFlag.setAttribute("src", data[0].flag)
+
+    countryInfo[0].innerHTML = "Native Name: " + data[0].nativeName
+    countryInfo[1].innerHTML = "Population: " + data[0].population.toLocaleString();
+    countryInfo[2].innerText += `Region: ${data[0].region}`
+    countryInfo[2].innerText += `\nSub-Region: ${data[0].subregion}`
+
+    data[0].currencies.forEach(e => {
+        const div = document.createElement("div");
+        const par = document.createElement("p");
+        par.innerText = `${e.name} {${e.symbol}}`;
+        div.appendChild(par)
+        countryCurrency.appendChild(div);
+    })
+
+    data[0].borders.forEach(e => {
+        data.forEach(x => {
+            if(e === x.alpha3Code) {
+                const div = document.createElement("div");
+                const par = document.createElement("p");
+                const img = document.createElement("img");
+                img.setAttribute("src", x.flag)
+                img.style.height = "75px";
+                div.appendChild(img)
+                par.innerText = `${x.name}`;
+                div.style.display = "flex";
+                div.appendChild(par)
+                countryBorders.appendChild(div);
+            }
+        });
+        
+    });
 }, 2000);
 
 
 
+
+// COVID SECTION
